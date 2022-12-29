f31e74c3c3a57ea299a366e63e7358ad5db00c24
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2022-12-29 19:53:49 +0000

    week1-Day2-exercicexp1

--- exercice1/script.js
+++ exercice1/script.js
@@ -1,6 +1,8 @@
-/* déclaration de variable*/
-const favorite = 'garba';
-const favoritebreak = 'monpetitdéjeuner';
-/* affichage*/
-console.log(' I eat '+ favorite + ' at very ' + favoritebreak);
-
+let x = 5;
+let y = 2;
+if(x>y){
+    console.log("x est le plus grand nombre");
+}
+else{
+    console.log('x n`èest petit pas le plus grand');
+}
