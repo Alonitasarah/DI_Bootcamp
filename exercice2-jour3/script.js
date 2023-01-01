86e155e6384ae4b85c55bcefafde37daaafce74d
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-01 09:21:15 +0000

    week2-Day3-exercicexp2

---
+++ exercice2-jour3/script.js
@@ -0,0 +1,7 @@
+const colors = ["rose","violet","bleu","noir","blanc"];
+const suffixes = ["er","ieme","ieme","ieme","ieme"];
+
+for (let i= 0; i< colors.length; i++){
+console.log(`Mon ${i+1}${suffixes[i]} choix est ${colors[i]}`);
+    
+}
