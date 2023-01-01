1d9dbc655940bfa9d00095aa9a142908ab3247dc
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-01 09:44:51 +0000

    week2-Day2-exercicexp5

---
+++ exercice5-jour3/script.js
@@ -0,0 +1,11 @@
+const famille = {
+    papa : "ZOZO",
+    monprenom :"sarah",
+    frere : "ibrahim",
+    soeur : "aicha"
+}
+for (const key in famille) {
+    console.log('key',key);
+    console.log('value::',famille[key]);
+           
+}
