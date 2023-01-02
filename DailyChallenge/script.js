434bbc4b11a79780922f5bdc235cece79a44d2bd
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-02 21:56:02 +0000

    week2-Day3-DailyChallenge

--- DailyChallenge/script.js
+++ DailyChallenge/script.js
@@ -1,12 +1,29 @@
-let sentence = ("He is not a bad formula driver.")
-let wordNot = sentence.indexOf("not"); 
-console.log(wordNot);
-let wordBad  = sentence.indexOf("bad");
-console.log(wordBad);
-if(wordBad > wordNot) {
-    const slice = sentence.slice(wordNot, wordBad + 3)
-    console.log(sentence.replace(slice,"good"))
-}else{
-    alert(sentence)
+let input = prompt("Entrez des  mots et séparé les avec des virgules");
+let MotsReournes = input.split(",");
+let LongueurMaximal = 0;
 
+let Etoile = '';
+ MotsReournes.forEach((i)=>{
+
+    if(LongueurMaximal < i.length)
+    {
+        LongueurMaximal = i.length
+    }
+});
+
+LongueurMaximaleEtoile = LongueurMaximal + 4;
+Etoile +=   "*".repeat(LongueurMaximaleEtoile);
+Etoile += "\n";
+let space = '';
+
+ for(let j=0; j < MotsReournes.length; j++)
+{
+    let Espace = LongueurMaximaleEtoile - MotsReournes[j].length - 3 ;
+    space = ' '.repeat(Espace)
+
+    Etoile +=  "* " + MotsReournes[j] + space + "* \n";
 }
+
+Etoile = Etoile + "*".repeat(LongueurMaximaleEtoile);
+
+ console.log(Etoile);
