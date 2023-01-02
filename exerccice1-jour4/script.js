6092c2722116ac77cf6c5155ddda3c558d0f3e7c
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-02 22:01:13 +0000

    Week2-Day4-exerciceXp1

---
+++ exerccice1-jour4/script.js
@@ -0,0 +1,13 @@
+function infoAboutMe()
+{
+    return console.log('Mon nom est ZOZO ALONITA SARAH. J"ai 21ans. Mon loisir la télé et réseaux sociaux');
+}
+
+
+function infoAboutPerson(personName, personAge, personFavoriteColor){
+   
+    return console.log(`mon nom est ${personName}, j"ai ${personAge} ans, ma couleur favorite est ${personFavoriteColor}”`);
+}
+
+infoAboutPerson("David", 45, "blue")
+infoAboutPerson("Josh", 12, "yellow");
